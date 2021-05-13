8a167dac02b5598447b9fae01f739969ae16043b
jacob0532 <61889488+jacob0532@users.noreply.github.com>
2021-05-13 11:44:07 -0600

    Ejemplo de sockets

---
+++ Ejemplo sockets/Sockets/src/sockets/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yeico
+ */
+package sockets;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    //192.168.0.11
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Servidor c = new Servidor();
+        c.iniciar();
+        
+    }
+    
+}
---
+++ Ejemplo sockets/Sockets/src/sockets/Servidor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sockets;
+
+/**
+ *
+ * @author yeico
+ */
+import java.net.*;
+import java.io.*;
+
+public class Servidor {
+    ServerSocket server;
+    Socket socket;
+    int puerto = 9000;
+    BufferedReader entrada;
+    DataOutputStream salida;
+    public void iniciar(){
+       try{
+           server = new ServerSocket(puerto);
+           socket = new Socket();
+           socket = server.accept();
+           
+           entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           String mensaje = entrada.readLine();
+           System.out.println(mensaje);
+           salida = new DataOutputStream(socket.getOutputStream());
+           salida.writeUTF("Adios Mundo");
+           socket.close();
+           
+       } catch(Exception e){}
+    }
+    
+    
+}
